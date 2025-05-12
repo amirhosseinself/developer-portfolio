a955ad02c465554107922723477137c4824daa82
amir-9 <aghaamirjan13@gmail.com>
2025-05-12 15:23:15 +0330

    .

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,27 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import AboutSection from "./components/homepage/about";
-import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
 
+const HeroSection = dynamic(
+  () => import("./components/homepage/hero-section"),
+  { ssr: false }
+);
+const AboutSection = dynamic(() => import("./components/homepage/about"), {
+  ssr: false,
+});
+const Experience = dynamic(() => import("./components/homepage/experience"), {
+  ssr: false,
+});
 const Skills = dynamic(() => import("./components/homepage/skills"), {
+  ssr: false,
+});
+const Projects = dynamic(() => import("./components/homepage/projects"), {
+  ssr: false,
+});
+const Education = dynamic(() => import("./components/homepage/education"), {
+  ssr: false,
+});
+const ContactSection = dynamic(() => import("./components/homepage/contact"), {
   ssr: false,
 });
 
