1941e81658bfd34a1e24cfb367e4dae1f4264fa5
amir-9 <aghaamirjan13@gmail.com>
2025-05-12 15:45:08 +0330

    .

--- app/page.jsx
+++ app/page.jsx
@@ -4,19 +4,15 @@ import dynamic from "next/dynamic";
 
 import HeroSection from "./components/homepage/hero-section";
 import AboutSection from "./components/homepage/about";
+import Projects from "./components/homepage/projects";
+import ContactSection from "./components/homepage/contact";
 const Experience = dynamic(() => import("./components/homepage/experience"), {
   ssr: false,
 });
 const Skills = dynamic(() => import("./components/homepage/skills"), {
   ssr: false,
 });
-const Projects = dynamic(() => import("./components/homepage/projects"), {
-  ssr: false,
-});
 const Education = dynamic(() => import("./components/homepage/education"), {
-  ssr: false,
-});
-const ContactSection = dynamic(() => import("./components/homepage/contact"), {
   ssr: false,
 });
 
