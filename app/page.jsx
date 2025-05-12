158a45d4f3ad02853297bd8c162876fa4899cfaa
amir-9 <aghaamirjan13@gmail.com>
2025-05-12 15:16:25 +0330

    .

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,16 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
+
+const Skills = dynamic(() => import("./components/homepage/skills"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
