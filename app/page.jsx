23497ca4bc66452d61998ca3be88cfc965ac0582
amir-9 <aghaamirjan13@gmail.com>
2025-05-13 20:18:43 +0330

    .

--- app/page.jsx
+++ app/page.jsx
@@ -6,17 +6,17 @@ import HeroSection from "./components/homepage/hero-section";
 import AboutSection from "./components/homepage/about";
 import Projects from "./components/homepage/projects";
 import ContactSection from "./components/homepage/contact";
+import Skills from "./components/homepage/skills";
 const Experience = dynamic(() => import("./components/homepage/experience"), {
   ssr: false,
 });
-import Skills from "./components/homepage/skills";
 const Education = dynamic(() => import("./components/homepage/education"), {
   ssr: false,
 });
 
 export default function Home() {
   return (
-    <div suppressHydrationWarning>
+    <div>
       <HeroSection />
       <AboutSection />
       <Experience />
