29dcbf28bd93e8eba7a9ac946cb0aa4e758f028d
amir-9 <aghaamirjan13@gmail.com>
2025-05-13 20:14:16 +0330

    .

--- app/page.jsx
+++ app/page.jsx
@@ -6,9 +6,11 @@ import HeroSection from "./components/homepage/hero-section";
 import AboutSection from "./components/homepage/about";
 import Projects from "./components/homepage/projects";
 import ContactSection from "./components/homepage/contact";
-import Experience from "./components/homepage/experience";
-import Education from "./components/homepage/education";
-const Skills = dynamic(() => import("./components/homepage/skills"), {
+const Experience = dynamic(() => import("./components/homepage/experience"), {
+  ssr: false,
+});
+import Skills from "./components/homepage/skills";
+const Education = dynamic(() => import("./components/homepage/education"), {
   ssr: false,
 });
 
