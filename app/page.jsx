6713ab895d45b5ccd9de6b7e2616d8e4e32c722d
amir-9 <aghaamirjan13@gmail.com>
2025-05-14 13:56:15 +0330

    .

--- app/page.jsx
+++ app/page.jsx
@@ -7,12 +7,8 @@ import AboutSection from "./components/homepage/about";
 import Projects from "./components/homepage/projects";
 import ContactSection from "./components/homepage/contact";
 import Skills from "./components/homepage/skills";
-const Experience = dynamic(() => import("./components/homepage/experience"), {
-  ssr: false,
-});
-const Education = dynamic(() => import("./components/homepage/education"), {
-  ssr: false,
-});
+import Experience from "./components/homepage/experience";
+import Education from "./components/homepage/education";
 
 export default function Home() {
   return (
